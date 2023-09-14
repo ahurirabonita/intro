aeeaac16f96f66478e3cb5a43a12c1cdfd8f2e38
Bonita <ahurirabonita@gmail.com>
2023-09-14 11:07:30 -0700

    get user input

---
+++ dart/collections.dart
@@ -0,0 +1,54 @@
+import "dart:io";
+
+String getUserInput(){
+    String? input = stdin.readLineSync();
+    if(input !=null){
+        return input;
+    }
+    return "invalid input";
+}
+
+String getUserName(){
+    print("EnterName");
+    String name = getUserInput();
+    print ("Name is:$name");
+    return name;
+}
+
+String getUserAge(){
+    print ("EnterAge");
+    String age = getUserInput();
+    print("Age is:$age");
+    return age;
+}
+
+String getUserAddress(){
+    print ("EnterAddress");
+    String address = getUserInput();
+    print("Address is:$address");
+    return address;
+}
+
+String getUserGender(){
+    print ("EnterGender");
+    String gender = getUserInput();
+    print("Gender is:$gender");
+    return gender;
+}
+
+String getUserCourse(){
+    print ("EnterCourse");
+    String course = getUserInput();
+    print("Course is:$course");
+    return course;
+}
+
+void main(){
+    getUserName();
+    getUserAge();
+    getUserAddress();
+    getUserGender();
+    getUserCourse();
+
+
+}
