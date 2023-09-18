8602304f269564d5921cc3cec2494daf7fe2d6bc
Bonita <ahurirabonita@gmail.com>
2023-09-18 12:12:10 -0700

    classes

---
+++ dart/classes.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<Student> students = [];
+  Student bonita = Student();
+  bonita.name = "Bonita";
+  bonita.age = 12;
+  bonita.address = "mbra";
+
+  Student rolah = Student();
+  rolah.name = "Rolah";
+  rolah.age = 18;
+  rolah.address = "kla";
+
+  students.addAll([bonita, rolah]);
+
+  for (var student in students) {
+    print(student.name);
+    print(student.age);
+    print(student.address);
+  }
+}
+
+class Student {
+  String? name;
+  int? age;
+  String? address;
+}
